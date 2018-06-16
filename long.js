0dce787020e548374c90efbb4fb19eed4b8bbbfd
Glitch (gentle-street) <none>
2018-06-16 22:21:20 +0000

    📕🌮 Updated with Glitch

--- long.js
+++ long.js
@@ -11,11 +11,11 @@ const squareE = (squareA - squareB) / squareA;
 const longLen = lat => a * Math.cos(toRadians(lat)) * Math.PI / (180 * n * Math.sqrt(1 - squareE * squareSin(lat)));
 const latLen = lat => (111132.954 - 559.882 * Math.cos(2 * toRadians(lat)) + 1.175 * Math.cos(4 * toRadians(lat))) / n;
 const ratio = lat => {
-    const ltl = latLen(lat);
-    const lgl = longLen(lat);
-    const x = Math.round(ltl / lgl * 100) / 100;
-    // console.log(x);
-    return x;
+  const ltl = latLen(lat);
+  const lgl = longLen(lat);
+  const x = Math.round(ltl / lgl * 100) / 100;
+  // console.log(x);
+  return x;
 };
 
 module.exports = {
